a8e935469d34b4b083e78ff22e9a60d4a269bedd
Yi Yang <weiy@bxscience.edu>
2019-08-13 09:59:39 -0400

    s

--- random.js
+++ random.js
@@ -2,7 +2,6 @@ var list = ["maze.html", "snake.html", "fortuneteller.html", "2048.html", "moodg
 
 
 function randomgame(){
-  console.log("test")
   // console.log();
   // the math function take a number randomly from 0-5 and ise the index to access the list
   window.location = list[Math.floor(Math.random() * 6)];
